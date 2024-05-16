ecd3530da4c2bb954558b21e9882cbef3f8a759d
Piotr <trojanpiotrek@interia.pl>
2024-05-16 19:27:25 +0200

    added full working offcanvas

--- js/main.js
+++ js/main.js
@@ -5,18 +5,36 @@ const transferType = document.querySelector("#transact-type");
 const paymentCard = document.querySelector(".payment-card");
 const transferCard = document.querySelector(".transfer-card");
 const depositCard = document.querySelector(".deposit-card");
-
-transferType.addEventListener("change", function(){
+const withdrawCard = document.querySelector(".withdraw-card");
+// Check For Transaction type Event Listener
+transferType.addEventListener("change", ()=>{
     // Check for Transaction type and Display Form:
     switch(transferType.value){
         case "payment":
+            depositCard.style.display = "none";
+            withdrawCard.style.display = "none";
             paymentCard.style.display = "block";
-            paymentCard.nextElementSibling.style.display = "none";
+            paymentCard.nextElementSibling.style.display = "none";    
             break;
         case "transfer":
+            withdrawCard.style.display = "none";
             transferCard.previousElementSibling.style.display = "none";
             transferCard.style.display = "block";
             transferCard.nextElementSibling.style.display = "none";
             break;
+        case "deposit":
+            paymentCard.style.display = "none";
+            depositCard.previousElementSibling.style.display = "none";
+            depositCard.style.display = "block";
+            depositCard.nextElementSibling.style.display = "none";
+            break;
+        case "withdraw":
+            paymentCard.style.display = "none";
+            transferCard.style.display = "none";
+            withdrawCard.previousElementSibling.style.display = "none";
+            withdrawCard.style.display = "block";
+        break;
     }
+    // End Of Check for Transaction type and Display Form:
 })
+// End Of Check For Transaction type Event Listener
